887f2f2f695629de4eca03d670c7872f60fcbdc3
FildonPrime <fildonprime@gmail.com>
2018-04-06 21:37:57 +0530

    updated kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -11,24 +11,46 @@ exports.run = (Bot, message, args) => {
     let user = message.mentions.users.first(); 
     let reason = args.slice(1).join(' ');
 
-    if (args < 1) return message.channel.send(`\`\`\`\n${settings.prefixyes}kick [user tag] <reason>\n\nkicks the mentioned user from the server.\n\`\`\``); //BAN HELP
+    if (args < 1) return message.channel.send(`\`\`\`\n${Bot.settings.prefix}kick [user tag] <reason>\n\nkicks the mentioned user from the server.\n\`\`\``); //BAN HELP
 
     if (message.mentions.users.size < 1) return message.reply('You must mention someone to kick').catch(console.error);
 
     if (!message.guild.member(user).kickable) return message.reply('cannot kick that member');
 
-    if (user.id === settings.ownerid) return message.reply('You cant Kick that person');
+    if (user.id === Bot.settings.ownerId) return message.reply('You cant Kick that person');
 
     if (user.id === message.author.id) return message.reply('SelfHarm is bad')
 
+    let guildMember = message.guild.members.get(user.id);
+
+    if(!guildMember) {
+        return Bot.emit('error', "Invalid Member", "Please recheck the member you tagged", message.channel);
+    }
+
+    guildMember.send({
+        embed: {
+            description: `You got kicked from ${message.guild.name} due to ${reason} by ${message.author.username}`,
+            color: 123456
+        }
+    })
+
+    guildMember.kick().then(() => {
+        message.channel.send({
+            embed: {
+                description: `Successfully kicked \`${guildMember.user.username}\``,
+                color: 123456
+            }
+        })
+    })
+
 }
 exports.config = {
-    aliases: ['h'],
+    aliases: ['kick'],
     enabled: true
 };
 
 exports.help = {
-    name: 'help',
+    name: 'kick',
     botPermission: 'KICK_MEMBERS',
     userPermission: 'KICK_MEMBERS',
     usage: 'rs3x7',
