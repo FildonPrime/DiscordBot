b56354b32dba5d8f7a9dbcf56d7eab81e524834e
FildonPrime <fildonprime@gmail.com>
2018-04-18 01:24:05 +0530

    reworked messageDelete event

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,4 +1,18 @@
 
 module.exports = async message => {
-    
+    let channelId = message.client.settings.logchannelId;
+
+    let channel = message.guild.channels.get(channelId);
+    let msg = await message.content;
+
+    var SqlQuery = "INSERT INTO logs (messageAuthor, messageContent, messageId) VALUES (?, ?, ?)"
+    var inserts = [message.author, msg, message.id]
+    SqlQuery = message.client.db.format(SqlQuery, inserts);
+    message.client.con.query(SqlQuery, function(e,r,f) {
+        if(e) {
+            console.log(e)
+            return message.client.emit('error', "MySql Error, Please mention the BOT author", message.channel)
+        }
+        channel.send("MESSAGE DELTED")
+    })
 }
